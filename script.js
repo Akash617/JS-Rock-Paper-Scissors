f6406f4a83afe45e13b19b022c0a3447b99c4151
root <akash617@yahoo.com>
2021-09-20 02:06:28 -0400

    Added score and a winner display

--- script.js
+++ script.js
@@ -19,14 +19,6 @@ function checkWinner(playerNum) {
   }
 }
 
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    let result = buttonClicked(button.id);
-  });
-});
-
 function displayResult(result) {
   let resultDisplayContainer = document.querySelector('.result_display_container');
   let resultDisplay = document.createElement('div');
@@ -36,9 +28,51 @@ function displayResult(result) {
     resultDisplay.textContent = "It's a tie!";
   } else if (result == 1) {
     resultDisplay.textContent = "You lost!";
+    computerScore++;
   } else {
     resultDisplay.textContent = "You won!";
+    playerScore++;
   }
 
   resultDisplayContainer.appendChild(resultDisplay);
 }
+
+function displayScore() {
+  let scoreContainer = document.querySelector('.score_container');
+  let scoreDisplay = document.createElement('div');
+  scoreDisplay.classList.add('score_display');
+
+  scoreDisplay.textContent = `Player score: ${playerScore} Computer score: ${computerScore}`;
+
+  while(scoreContainer.firstChild) {
+    scoreContainer.removeChild(scoreContainer.firstChild);
+  }
+
+  scoreContainer.appendChild(scoreDisplay);
+}
+
+function displayWinner() {
+  let winnerContainer = document.querySelector('.winner_container');
+  let winnerDisplay = document.createElement('div');
+  winnerDisplay.classList.add('winner_display');
+
+  if (playerScore ==5) {
+    winnerDisplay.textContent = "You won the game!";
+  } else if (computerScore == 5) {
+    winnerDisplay.textContent = "The computer won the game!";
+  }
+
+  winnerContainer.appendChild(winnerDisplay);
+}
+
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let result = buttonClicked(button.id);
+    displayScore();
+    displayWinner();
+  });
+});
